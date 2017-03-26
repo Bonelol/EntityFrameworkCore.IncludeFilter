8cfe5503fc81763dba160db04234bb98e0b2637a
max.meng <max.meng@PC-ITSDEV-1310>
2017-03-27 11:03:42 +1300

    Add project files.

---
+++ src/EntityFrameworkCore.IncludeFilter/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCore.IncludeFilter
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+    }
+}
